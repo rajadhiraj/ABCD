4bb17012338d6dfd60d94b505e961e5c542fe215
Manav <Manav@DESKTOP-C89ERP2>
2020-04-13 16:48:18 +0530

    b01

---
+++ ABCD/src/test/java/com/cubexo/ABC_April_Project/CloseApplication.java
@@ -0,0 +1,13 @@
+package com.cubexo.ABC_April_Project;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CloseApplication {
+
+	@Test
+	public void closeApplication()
+	{
+		Reporter.log("Close The Application",true);
+	}
+}
---
+++ ABCD/src/test/java/com/cubexo/ABC_April_Project/OpenBrowser.java
@@ -0,0 +1,14 @@
+package com.cubexo.ABC_April_Project;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class OpenBrowser {
+
+	
+	@Test
+	public void openBrowser()
+	{
+	Reporter.log("please ho ja yaar",true);	
+	}
+}
